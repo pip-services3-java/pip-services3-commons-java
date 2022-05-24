5819cea04826a1347fea5a5eee7b5df870448828
Dmitry Krayniy <krdima92@gmail.com>
2022-05-24 21:20:39 +0300

    Updated release script logic

--- release.ps1
+++ release.ps1
@@ -11,46 +11,28 @@ if ($component.version -ne $version) {
     throw "Versions in component.json and pom.xml do not match"
 }
 
+# Use existing gpg key if env variables set 
+if (($env:GPG_PUBLIC_KEY -ne $null) -and ($env:GPG_PRIVATE_KEY -ne $null)) {
+   Set-Content -Path "gpg_public.key" -Value $env:GPG_PUBLIC_KEY
+   Set-Content -Path "gpg_private.key" -Value $env:GPG_PRIVATE_KEY
+
+   Write-Host "Before import"
+   gpg --list-keys
+   gpg --import "gpg_public.key"
+   gpg --batch --passphrase "$($env:GPG_PASSPHRASE)" --import "gpg_private.key"
+   Write-Host "After import"
+   gpg --list-keys
+
+   # Remove m2 config for clean run
+   if (Test-Path "~/.m2/settings.xml") {
+      Remove-Item -Path "~/.m2/settings.xml" -Force
+   }
+} 
+
 # Create ~/.m2/settings.xml if not exists
 if (!(Test-Path "~/.m2/settings.xml")) {
-   # Use existing gpg key if env variables set 
-   if (($env:GPG_PUBLIC_KEY -ne $null) -and ($env:GPG_PRIVATE_KEY -ne $null)) {
-      Set-Content -Path "gpg_public.key" -Value $env:GPG_PUBLIC_KEY
-      Set-Content -Path "gpg_private.key" -Value $env:GPG_PRIVATE_KEY
-
-      Write-Host "Before import"
-      gpg --list-keys
-      gpg --import "gpg_public.key"
-      gpg --batch --passphrase "$($env:GPG_PASSPHRASE)" --import "gpg_private.key"
-      Write-Host "After import"
-      gpg --list-keys
-
-   } else {
-      # Generate new gpg key
-      $genKey = @"
-Key-Type: 1
-Key-Length: 2048
-Subkey-Type: 1
-Subkey-Length: 2048
-Name-Real: $($env:GPG_USERNAME)
-Name-Email: $($env:GPG_EMAIL)
-Passphrase: $($env:GPG_PASSPHRASE)
-Expire-Date: 0
-"@
-
-      Set-Content -Path "genKey" -Value $genKey
-      
-      $gpgOut = gpg --batch --gen-key genKey
-
-      # Get gpg keyname
-      $gpgKeyname = Read-Host "Enter gpg key id"
-
-      # Register gpg
-      gpg --keyserver keyserver.ubuntu.com --send-keys $gpgKeyname
-   }
-
    # Create m2 config
-    $m2SetingsContent = @"
+   $m2SetingsContent = @"
 <?xml version="1.0" encoding="UTF-8"?>
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -89,10 +71,11 @@ Expire-Date: 0
 "@
 
    # Save config
-    if (!(Test-Path "~/.m2")) {
-        $null = New-Item -Path "~/.m2" -ItemType "directory"
-    }
-    Set-Content -Path "~/.m2/settings.xml" -Value $m2SetingsContent
+   if (!(Test-Path "~/.m2")) {
+      $null = New-Item -Path "~/.m2" -ItemType "directory"
+   }
+   Set-Content -Path "~/.m2/settings.xml" -Value $m2SetingsContent
+   Write-Host "'~/.m2/settings.xml' created"
 }
 
 Write-Host "Getting mvn version..."
