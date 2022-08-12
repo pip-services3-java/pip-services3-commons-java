76aa655a5729b6548b05c6ccbc4a06530b379eac
Dmitry Krayniy <krdima92@gmail.com>
2022-08-12 15:57:17 +0400

    Removed unused images

--- clean.ps1
+++ clean.ps1
@@ -5,15 +5,11 @@ $component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
 $buildImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
 $docsImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
 $testImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
-$rcImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)"
-$latestImage = "$($component.registry)/$($component.name):latest"
 
 # Remove docker images
 docker rmi $buildImage --force
 docker rmi $docsImage --force
 docker rmi $testImage --force
-docker rmi $rcImage --force
-docker rmi $latestImage --force
 docker rmi -f $(docker images -f "dangling=true" -q) # remove build container if build fails
 docker image prune --force
 
